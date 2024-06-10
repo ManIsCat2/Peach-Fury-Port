c9a7b3fbcfbf102aebdab118432adc8245c53c1e
ThePerfectLuigi64 <ihlahyconley+multipliez@gmail.com>
2024-06-10 16:24:52 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
 -- name: Peach's Fury
-<<<<<<< HEAD
 -- incompatible: romhack
 
 gLevelValues.entryLevel = LEVEL_HMC
-=======
 -- description: \\#ff0000\\WARNING:\nTHE FOLLOWING MOD SHOULD NOT BE HOSTED PUBLICLY AS IT'S ONLY FOR TESTING!!\nIF YOU SEE THIS MESSAGE IN A PUBLIC SERVER, PLEASE REPORT IT TO A MODERATOR IN THE SM64EXCOOP SERVER!\n\n \\#f7f7f7\\Peach's Fury is a romhack made by \n Kaze \n & \n thelegendofzenia \n and ported by ThePerfectMario64 \n this hach was possible \n cause Kaze was nice enough \n to open-source Peach's Fury, And the models.
 -- incompatible: romhack
 
@@ -13,5 +11,3 @@ gLevelValues.entryLevel = LEVEL_HMC
 
 gLevelValues.entryLevel = LEVEL_HMC
 gLevelValues.exitCastleLevel = LEVEL_HMC
-
->>>>>>> 32c9be1aeee099e20b254a4eeac8c38f53671777
