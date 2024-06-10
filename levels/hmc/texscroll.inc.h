96072edb4a6d0b9f79f67c83d9b0320b4f90a88c
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-06-11 00:51:52 +0330

    llll

--- levels/hmc/texscroll.inc.h
+++
@@ -1,4 +0,0 @@
-extern void scroll_gfx_mat_hmc_dl_Watermat_001_layer0();
-extern void scroll_gfx_mat_hmc_dl_f3d_material_204_layer5();
-extern void scroll_gfx_mat_hmc_dl_f3d_material_019_layer1();
-extern void scroll_hmc();
