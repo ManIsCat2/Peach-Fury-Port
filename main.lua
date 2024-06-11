e114a29b2c5400402ab5794843c908309ed35b5b
ThePerfectLuigi64 <ihlahyconley+multipliez@gmail.com>
2024-06-10 20:21:48 -0500

    Remove Repeat Code

--- main.lua
+++ main.lua
@@ -1,8 +1,6 @@
 -- name: Peach's Fury
 -- incompatible: romhack
 
-gLevelValues.entryLevel = LEVEL_HMC
-
 -- description: \\#ff0000\\WARNING:\nTHE FOLLOWING MOD SHOULD NOT BE HOSTED PUBLICLY AS IT'S ONLY FOR TESTING!!\nIF YOU SEE THIS MESSAGE IN A PUBLIC SERVER, PLEASE REPORT IT TO A MODERATOR IN THE SM64EXCOOP SERVER!\n\n \\#f7f7f7\\Peach's Fury is a romhack made by \n Kaze \n & \n thelegendofzenia \n and ported by ThePerfectMario64 \n this hach was possible \n cause Kaze was nice enough \n to open-source Peach's Fury, And the models.
 -- incompatible: romhack
 
