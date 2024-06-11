f38b2e9ed39624adebb03ec470fbbac752ef3532
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-06-11 17:20:00 +0330

    bouncy flower

--- custom_bhvs.lua
+++ custom_bhvs.lua
@@ -1,4 +1,4 @@
-local towerPos = {1361.0, 861.0, 1111.0, 361.0, 611.0, -139.0, 111.0}
+local towerPos = { 1361.0, 861.0, 1111.0, 361.0, 611.0, -139.0, 111.0 }
 
 function goombastar(o)
     if (o.oBehParams == 6) then
@@ -16,4 +16,30 @@ function goombastar(o)
     end
 end
 
+function lua_bounce_off_object(m, o, velY)
+    m.pos.y = o.oPosY + o.hitboxHeight
+    m.vel.y = velY
 
+    m.flags = m.flags & ~MARIO_UNKNOWN_08
+
+    play_sound(SOUND_ACTION_BOUNCE_OFF_OBJECT, m.marioObj.header.gfx.cameraToObject)
+end
+
+local latdist = 120.0
+local vertdist = 80.0
+
+MODEL_BOUNCY_FLOWER = smlua_model_util_get_id("bouncyflower_geo")
+
+function bounceyflower(o)
+    smlua_anim_util_set_animation(o, "anim_spin_flower")
+    o.oFaceAngleYaw = o.oFaceAngleYaw + 0x800
+    local lateral_dist = lateral_dist_between_objects(o, gMarioStates[0].marioObj)
+    local vertical_dist = math.abs(gMarioStates[0].pos.y - o.oPosY)
+    if (lateral_dist < latdist) and (vertical_dist < vertdist) then
+        gMarioStates[0].faceAngle.x = 0
+        play_sound(SOUND_MARIO_TWIRL_BOUNCE, gMarioStates[0].marioObj.header.gfx.cameraToObject)
+        drop_and_set_mario_action(gMarioStates[0], ACT_TWIRLING, 0)
+        lua_bounce_off_object(gMarioStates[0], o, 100.0 - o.oBehParams2ndByte)
+        gMarioStates[0].vel.y = 100.0 - o.oBehParams2ndByte
+    end
+end
--- data/behavior_data.c
+++ data/behavior_data.c
@@ -6,3 +6,17 @@ const BehaviorScript bhvGoombaStar[] = {
         CALL_NATIVE(goombastar),
     END_LOOP(),
 };
+
+const BehaviorScript bhvBounceFlower[] = {
+    BEGIN(OBJ_LIST_GENACTOR),
+    ID(id_bhvNewId),
+    OR_INT(oFlags, (OBJ_FLAG_COMPUTE_DIST_TO_MARIO | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
+    SET_HITBOX(120, 50),
+    SCALE(0, 150),
+    SET_INTERACT_TYPE(INTERACT_BOUNCE_TOP),
+    SET_INT(oIntangibleTimer, 0),
+    BEGIN_LOOP(),
+    SET_INT(oInteractStatus, 0),
+    CALL_NATIVE(bounceyflower),
+    END_LOOP(),
+};
