7c42677c118a621d4e60a6a6d2c967c7b676ae8a
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-06-11 17:39:40 +0330

    star dance music

--- data/behavior_data.c
+++ data/behavior_data.c
@@ -13,7 +13,6 @@ const BehaviorScript bhvBounceFlower[] = {
     OR_INT(oFlags, (OBJ_FLAG_COMPUTE_DIST_TO_MARIO | OBJ_FLAG_UPDATE_GFX_POS_AND_ANGLE)),
     SET_HITBOX(120, 50),
     SCALE(0, 150),
-    SET_INTERACT_TYPE(INTERACT_BOUNCE_TOP),
     SET_INT(oIntangibleTimer, 0),
     BEGIN_LOOP(),
     SET_INT(oInteractStatus, 0),
--- main.lua
+++ main.lua
@@ -23,6 +23,7 @@ gServerSettings.stayInLevelAfterStar = 1
 -----------------
 
 audio_0A = audio_stream_load("0A_domeL.mp3")
+audio_02 = audio_stream_load("02_stardanceleft.mp3")
 audio_stream_set_looping(audio_0A, true)
 audio_stream_play(audio_0A, true, 1)
 
@@ -31,9 +32,29 @@ audio_stream_play(audio_0A, true, 1)
 ------------------
 
 function breakable_box_init(o)
-    obj_scale_xyz(o,2,2,2)
+    obj_scale_xyz(o, 2, 2, 2)
     o.oCollisionDistance = 1000
 end
 
 hook_behavior(id_bhvBreakableBox, OBJ_LIST_SURFACE, false, breakable_box_init, nil)
 
+--------------------
+-- general things --
+--------------------
+
+played = false
+
+function mario_update(m)
+    if m.action == ACT_STAR_DANCE_NO_EXIT and not played then
+        play_music(SEQ_PLAYER_ENV, 0, 0);
+        audio_stream_set_looping(audio_02, false)
+        audio_stream_play(audio_02, false, 1)
+        played = true
+    end
+
+    if m.action ~= ACT_STAR_DANCE_NO_EXIT then
+        played = false
+    end
+end
+
+hook_event(HOOK_MARIO_UPDATE, mario_update)
