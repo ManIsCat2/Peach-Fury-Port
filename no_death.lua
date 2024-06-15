bf6923d93906f37864ddbd786704f2e15b954d53
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-06-15 22:09:24 +0330

    wtf i discovered glitch last second lmao

--- no_death.lua
+++ no_death.lua
@@ -10,17 +10,20 @@ function save_pos_update(m)
     local e = gMarioStateExtras[m.playerIndex]
     m.peakHeight = m.pos.y
     --if m.playerIndex ~= 0 then return end
-    if m.floor.type ~= SURFACE_DEEP_QUICKSAND and  m.floor.type ~= SURFACE_SLIPPERY then
-        if m.action == ACT_IDLE or m.action == ACT_WALKING then
-            vec3f_copy(e.saved_pos, m.pos)
+
+    if m.floor then
+        if m.floor.type ~= SURFACE_DEEP_QUICKSAND and m.floor.type ~= SURFACE_SLIPPERY then
+            if m.action == ACT_IDLE or m.action == ACT_WALKING then
+                vec3f_copy(e.saved_pos, m.pos)
+            end
         end
     end
-    if m.floor.type == SURFACE_DEEP_QUICKSAND and m.pos.y == m.floorHeight then
+    if m.floor and m.floor.type == SURFACE_DEEP_QUICKSAND and m.pos.y == m.floorHeight then
         m.action = ACT_DYING_QUICKSAND
         e.saved_transition_timer = e.saved_transition_timer + 1
         if e.saved_transition_timer > 20 then
             if m.playerIndex == 0 then
-            play_transition(WARP_TRANSITION_FADE_FROM_MARIO, 30, 0, 0, 0)
+                play_transition(WARP_TRANSITION_FADE_FROM_MARIO, 30, 0, 0, 0)
             end
             m.vel.y = 0
             mario_set_forward_vel(m, 0)
@@ -34,4 +37,4 @@ function save_pos_update(m)
 end
 
 hook_event(HOOK_MARIO_UPDATE, save_pos_update)
-hook_mario_action(ACT_DYING_QUICKSAND, { every_frame = act_dying_quicksand})
+hook_mario_action(ACT_DYING_QUICKSAND, { every_frame = act_dying_quicksand })
