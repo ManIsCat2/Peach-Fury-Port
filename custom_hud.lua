d90b692b336fe0464515ef8ce13e75af01118303
ManIsCat2 <137772623+ManIsCat2@users.noreply.github.com>
2024-06-15 16:08:58 +0330

    adapt to screen

--- custom_hud.lua
+++ custom_hud.lua
@@ -1,17 +1,16 @@
 local coinx = 322+60 + 15
 
 function render_stars_and_coin()
+    local m = gMarioStates[0]
     djui_hud_render_texture(gTextures.star, 8, 4, 1, 1)
     djui_hud_print_text("@", 24, 4, 1)
     djui_hud_print_text(tostring(gMarioStates[0].numStars), 40, 4, 1)
 
-    djui_hud_render_texture(gTextures.coin, (68 * 4.5) + 60, 4, 1, 1)
-    if gMarioStates[0].numCoins < 100 then
-        djui_hud_print_text("@", 322 + 60, 4, 1)
-    else
-        coinx = 322+60
-    end
-    djui_hud_print_text(tostring(gMarioStates[0].numCoins), coinx, 4, 1)
+    djui_hud_render_texture(gTextures.coin, djui_hud_get_screen_width() - 62, 4, 1,1)
+        if m.numCoins < 100 then
+            djui_hud_print_text("@", djui_hud_get_screen_width() - 46, 4, 1)
+        end
+        djui_hud_print_text(tostring(m.numCoins), djui_hud_get_screen_width() - (32 + (m.numCoins < 100 and 0 or 14)), 4, 1)
 end
 
 function on_behind_hud()
